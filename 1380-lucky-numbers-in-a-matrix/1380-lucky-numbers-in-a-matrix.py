32928008fec5f3bd6004530f472293bbde601182
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2022-08-19 17:48:28 +0530

    Time: 202 ms (59.93%), Space: 32.3 MB (5.17%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        A=np.array(matrix)
+        return list(set(A.max(axis=0)).intersection(A.min(axis=1)))
+        # minArray = set(min(i) for i in matrix)
+        # maxArray = set()
+        # for j in range(len(matrix)):
+        #     for k in range(len(matrix[0])):
+        #         temp = set()
+        #     maxArray.add(max(i) for i in matrix[j][k])
+        # print(minArray, maxArray)
