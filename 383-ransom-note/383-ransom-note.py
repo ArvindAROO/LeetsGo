cbc1d35e0ae67ce7596e818e6ac3a935f6664ca4
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2022-08-19 09:30:40 +0530

    Time: 44 ms (97.53%), Space: 14.2 MB (53.79%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: list, magazine: list) -> bool:
+        for i in ransomNote:
+            if i not in magazine:
+                return False
+        map1 = {}
+        map2 = {}
+        map1 = {i:ransomNote.count(i) for i in set(ransomNote)}
+        map2 = {i:magazine.count(i) for i in set(magazine)}
+        for i in map1:
+            if map1[i] > map2[i]:
+                return False
+        return True
