6a551f86b1a3269cc51c9d4685a8f822343df140
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2022-08-20 11:23:31 +0530

    Time: 674 ms (43.12%), Space: 139 MB (31.47%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,22 +1,31 @@
-class Solution:
-    def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
-        i = 0 
-        sol = ""
-        maps = {j[0]:j[1] for j in knowledge}
-        while i < len(s):
-            if s[i] == "(":
-                i += 1
-                copy = i
-                temp = ""
-                while s[i] != ")":
-                    temp += s[i]
-                    i+=1
-                # print(temp, knowledge)
-                if temp in maps:
-                    sol += maps[temp]
-                else:
-                    sol += "?"
-            else:
-                sol += s[i]
-            i+=1
-        return sol
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> maps;
+        for(auto i : knowledge)
+            maps[i[0]] = i[1];
+        int i = 0;
+        string sol = "";
+        while (i < s.size()){
+            if (s[i] == '('){
+                string temp = "";
+                i++;
+                while (s[i] != ')'){
+                    temp += s[i];
+                    i++;
+                }
+                if (maps.find(temp) != maps.end()){
+                    sol += maps[temp];
+                } else {
+                    sol += "?";
+                }
+            } else {
+                sol += s[i];
+            }
+            
+            i++;
+            
+        }
+        return sol;
+    }
+};
