27579940bd4448eb2671a8af1784522e54cb5601
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2022-08-24 06:37:08 +0530

    Time: 425 ms (47.20%), Space: 135.9 MB (47.96%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,87 @@
+// class Solution {
+    
+// public:
+//     int mem[50001][2][2];
+
+//     int sol(vector<int> &stones, bool Alice, bool Bob, int curr){
+//         if (curr >= stones.size()) return 0;
+//         int sum = 0, ans = INT_MIN;
+//         if(mem[curr][Alice][Bob] != -1) return mem[curr][Alice][Bob];
+        
+//         if (Alice) {
+//             for(int i = curr; i < curr + 3 && i < stones.size(); i++){
+//                 cout << "In alice"<< endl;
+//                 sum += stones[i];
+//                 ans = max(ans, sum +  sol(stones, false, true, i + 1));
+//             }
+//         }
+//         if (Bob) {
+//             for(int i = curr; i < curr + 3 && i < stones.size(); i++){
+//                 cout << "IN Bob" << endl;
+//                 sum += stones[i];
+//                 ans = max(ans, sum +  sol(stones, true, false, i + 1));
+//             }
+//         }
+//         return mem[curr][Alice][Bob] = ans;
+//         cout << "'" << ans << "'" << endl;
+//         return ans;
+//     }
+    
+//     string stoneGameIII(vector<int>& stoneValue) {
+//         // stones = stoneValue;
+//         // size = stoneValue.size();
+//         memset(mem, -1, sizeof mem);
+//         int total = 0, Alice = sol(stoneValue, true, false, 0);
+//         for(auto i : stoneValue) total += i;
+//         cout << total << " " << Alice;
+//         if(total - Alice < Alice) return "Alice";
+//         else if (total - Alice > Alice) return "Bob";
+//         return "Tie";
+//     }
+// };
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int dp[50001][2][2];
+    
+    int playGame(vector<int>& stones, bool alice, bool bob, int i) { 
+        if (i >= stones.size()) return 0;        
+        int ans;
+        int sum = 0;
+        
+        if (dp[i][alice][bob] != -1) return dp[i][alice][bob];
+        
+        if (alice) {
+            ans = INT_MIN;
+            for (int idx=i; idx < i + 3 && idx < stones.size(); idx++) {
+                sum += stones[idx];
+                ans = max(ans, sum + playGame(stones, false, true, idx + 1));
+            }
+        }
+        
+        if (bob) {
+            ans = INT_MAX;
+            for (int idx=i; idx < i + 3 && idx < stones.size(); idx++) {
+                sum += stones[idx];
+                ans = min(ans, playGame(stones, true, false, idx + 1));
+            }
+        }
+        
+        return dp[i][alice][bob] = ans;
+    }
+    
+    string stoneGameIII(vector<int>& stoneValue) {
+        memset(dp, -1, sizeof dp);
+        int totalScore = 0;
+        for (auto i : stoneValue) totalScore += i;
+        int aliceScore = playGame(stoneValue, true, false, 0);
+        if (totalScore - aliceScore > aliceScore)
+            return "Bob";
+        else if (totalScore - aliceScore < aliceScore)
+            return "Alice";
+        else return "Tie";
+    }
+};
