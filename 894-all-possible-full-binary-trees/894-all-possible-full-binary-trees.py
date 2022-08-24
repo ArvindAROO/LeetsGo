644c9552b6a043f321e5bbbf19e5260346e4ecd8
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2022-08-24 15:59:58 +0530

    Time: 322 ms (37.20%), Space: 18.4 MB (52.51%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.py
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.py
@@ -1,13 +1,16 @@
 class Solution:
+    def __init__(self):
+        self.maps = {}
     @lru_cache(None)
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
         if n == 1:
             return [TreeNode()]
-        
+        if n in self.maps:
+            return self.maps[n]
         ans = []
         for i in range(1, n-1, 2):
             for left in self.allPossibleFBT(i):
                 for right in self.allPossibleFBT(n-1-i):
                     ans.append(TreeNode(left=left, right=right))
-        
+        self.maps[n] = ans
         return ans
